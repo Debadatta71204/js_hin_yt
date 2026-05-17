ff9fd2866508d6290a697c5488f0b2213a057309
Debadatta71204 <debadatta71204@gmail.com>
2026-05-17 23:20:46 +0000

    update js basics files

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all j code as newer version
+//alert(3+3) we r using nodejs not browser
+//documentation: recommended mdn for ease. riginal documentation:ecma
+let name="hi"
+let age=18
+let isLogg=false
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//null:standalone value. representation of empty value
+//undefined:value not defined
+//symbol=>unique
+console.log(typeof name)
+console.log(typeof null)//object
+//speification on ecma
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
 console.log("hello world")
+const accountId=123
+let accEmail="hi"
+var accPass="123pass"
+accountCity="jaipur"
+// accountId=2:not allowed
+console.log(accountId)
+console.table([accEmail,accPass,accountCity])
+/*prefer not using var,for scope problem*/
+let accState //value undefined if logged
+
